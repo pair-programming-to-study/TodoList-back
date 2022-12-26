255b032feb693b8963800ac7a985f2b5720f6956
wellbeing-dough <kdw990202@naver.com>
2022-12-27 03:00:40 +0900

    초기세팅

---
+++ src/main/java/com/example/pairprogrammingtostudy/PairProgrammingToStudyApplication.java
@@ -0,0 +1,14 @@
+package com.example.pairprogrammingtostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairProgrammingToStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PairProgrammingToStudyApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/java/com/example/pairprogrammingtostudy/PairProgrammingToStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pairprogrammingtostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairProgrammingToStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
